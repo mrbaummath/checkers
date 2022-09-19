883da7b733993b01b8560814ce7e6c720c7410a3
mrbaummath <baum1286@gmail.com>
2022-09-19 16:24:25 -0400

    tokens are drawin in as divs within boxes. each token has a type which will track whether it is a regular piece or a kinged piece.

--- js/app.js
+++ js/app.js
@@ -14,6 +14,18 @@ const boardState = [
     []
 ]
 
+//function to fill tokens in game board. Takes row numbers and a color.
+const fillTokens = (firstRow, lastRow, color) => {
+    rows.slice(firstRow,lastRow + 1).forEach((row) => {
+        row.childNodes.forEach((box) => {
+            let token = document.createElement('div')
+            token.classList.add('token', `token-${color}`)
+            token.dataset.type = 'normal'
+            box.appendChild(token)
+        })
+    })
+}
+
 //function to draw the gameboard
 const drawGameBoard = () => {
     //create grid. Start by creating 8 rows to append in the board and then iterate to create 8 squares per row
@@ -24,6 +36,7 @@ const drawGameBoard = () => {
         gameBoard.appendChild(row)
         rows.push(row)
     }
+    //rows are done, now for each row iteratively create 8 squares.
     rows.forEach((row,rowNumber) => {
         for (let columnNumber =1; columnNumber < 9; columnNumber++) {
             const box = document.createElement('div')
@@ -33,11 +46,12 @@ const drawGameBoard = () => {
             row.appendChild(box)
         }
     })
-
     //color squares
+    console.log(rows)
     //add token images to appropriate squares
+    fillTokens(0,2,'red')
+    fillTokens(5,7,'black')
     //add event listeners to tokens
-
 }
 
 document.addEventListener('DOMContentLoaded', () => {
