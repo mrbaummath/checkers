f22666db496050f975d162c8fb807a9fba0ed2e5
mrbaummath <baum1286@gmail.com>
2022-09-27 08:54:51 -0400

    working on moving the move function to be a token method

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 
 const tokens = []
 const boxArray = [[],[],[],[],[],[],[],[]]
-const activeToken = null
+let activeToken = null
 
 //grab the div containing the game grid 
 const gameBoard = document.querySelector('#game-board')
@@ -66,7 +66,7 @@ class Token {
         return this.findValidMoves.captures
     }
     get allMoves() {
-        return this.findValidMoves
+        return concat(this.findValidMoves.adjacent, this.findValidMoves.capture)
     }
 
     makeNode() {
@@ -118,6 +118,14 @@ class Token {
         return boxArray[rowCheck][colCheck]
 
     }
+
+    move(boxNode) {
+        const box = boxArray[boxNode.dataset.row][boxNode.dataset.column]
+        if (this.validMovesAdj.includes(box)) {
+
+        }
+
+    }
    
 }
 
@@ -126,10 +134,6 @@ class Token {
 
 
 //below is original code
-
-
-
-
 
 const newGame = () => {
     rows.length = 0
@@ -219,20 +223,20 @@ const setToken = (event) => {
             takerToken = false
         }
         activeToken = tokenObj
-        tokenObj.isActive = true
-        activeToken.node.style.border='thick solid orange'
-        console.log("token is ready to be moved")
+        activeToken.isActive = true
+        activeToken.node.classList.add('token-active')
     //if the token clicked is already the active token, un-ready it for mvoement and set active token to none 
-    } else if (event.target === activeToken) {
-        activeToken.style.border='none'
+    } else if (tokenObj.isActive) {
+        activeToken.node.classList.remove('token-active')
+        activeToken.isActive = false
         activeToken = null
-        console.log("active token has been unclicked")
-    //if clicked token is not the active token, do nothing
-    } else if (clickedColor !== currentPlayer) {
-        console.log('wait your turn!')
-    } else {
-        console.log('there is already an active token')
     }
+    //if clicked token is not the active token, do nothing
+    // } else if (clickedColor !== currentPlayer) {
+    //     console.log('wait your turn!')
+    // } else {
+    //     console.log('there is already an active token')
+    // }
 
 }
 
@@ -252,72 +256,11 @@ const translate = (movedNode,destinationNode) => {
 const moveToken = (event) => {
     event.stopPropagation()
     //check for an active token. Only call to the function for seeing if this is a valid move if there is
-    const boxToken = event.target.firstChild 
-    //only act on empty squares when there is an active token
-    if (activeToken && !boxToken) {
-        //grab data and row values to update the board state and the data on the moved token
-        const oldRow = parseInt(activeToken.dataset.row)
-        const oldCol = parseInt(activeToken.dataset.column)
-        const newRow = parseInt(event.target.dataset.row)
-        const newCol = parseInt(event.target.dataset.column)
-        const color = activeToken.dataset.color
-        //grab arrays of box elements which are valid to be moved into, whether because they are empty and adjacent or because they represent where the token moves to after a capture
-        const validMoves = findValidMoves(activeToken)
-        const validAdjacent = validMoves.adjacent
-        let validCapture = validMoves.capture 
-        //if the move represents a capture, remove the captured piece from the board (add it to opposing player's captured pile) and move the token
-        if (validCapture && validCapture.boxes.includes(event.target)) {
-            takerToken = activeToken
-            const capturedToken = validCapture.tokens[validCapture.boxes.indexOf(event.target)]
-            //before removing the captured token, grab some data to update boardState
-            const capturedRow = parseInt(capturedToken.dataset.row)
-            const capturedCol = parseInt(capturedToken.dataset.column)
-            const capturedColor = capturedToken.dataset.color
-            boardState[capturedRow][capturedCol] = 'e'
-            const taker = capturedColor === 'black' ? 'red' : 'black'
-            capturedBy[taker] += 1
-
-            //append the captured token into the opposing player's zone and remove it's event listener
-            const zone = capturedColor === 'red' ? blackCapture : redCapture
-            const capturedTokenDiv = document.createElement('div')
-            capturedToken.removeEventListener('click',setToken)
-            const boxStyle = getComputedStyle(document.querySelector('.box'))
-            //assign the div containing the captured token to have the current height and width of a gameboard div. This will make it so the tokens in the capture area have the size of the game tokens (at least when captured)
-            capturedTokenDiv.classList.add('capture-box')
-            capturedTokenDiv.style.height = boxStyle.getPropertyValue('height')
-            capturedTokenDiv.style.width = boxStyle.getPropertyValue('width')
-            zone.appendChild(capturedTokenDiv)
-            //call translate function
-            translate(capturedToken,capturedTokenDiv)
-            //append token to capture area
-            // capturedTokenDiv.appendChild(capturedToken)
-
-        }
-        //if move is not a capture, make sure there has not already been a token used to make a capture during this turn. If the move is a capture, it doesn't matter so allow the move.
-        if ((validAdjacent.includes(event.target) && !takerToken) || validCapture.boxes.includes(event.target)) {
-            translate(activeToken,event.target)
-            // event.target.appendChild(activeToken)
-            setTimeout(()=>{activeToken.dataset.row = newRow
-            activeToken.dataset.column = newCol
-            boardState[newRow][newCol] = activeToken.dataset.color
-            boardState[oldRow][oldCol] = 'e'
-            //check if the active token needs king status and apply it if need
-            if ((color === 'black' && newRow === 7) || (color ==='red' && newRow === 0)) {
-                activeToken.dataset.type = 'king'
-                activeToken.classList.add('king')
-
-            }
-            lastPlayer = activeToken.dataset.color
-            activeToken.style.border = 'none'
-            activeToken = null
-            },700)
-           
-        }
-        //check for a win condition being met
-        setTimeout( () => {
-            checkWin()
-        }, 750)
+   if (activeToken) {
+    if (activeToken.allMoves.includes(event.target)) {
+        activeToken.move(event.target)
     }
+   }
 }
 
 //function to generate the gameboard and add event listeners to the boxes
